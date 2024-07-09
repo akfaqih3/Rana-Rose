e7334c43d476a1225378358519e876df05be1837
unknown <akfaqih3@gmail.com>
2024-07-09 07:44:06 -0700

    befor cart app

--- RanaRose/settings.py
+++ RanaRose/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'RanaRose',
     'Site',
     'Product',
-    'Order',
 ]
 
 MIDDLEWARE = [
--- RanaRose/static/js/script.js
+++ RanaRose/static/js/script.js
@@ -7,25 +7,3 @@ for (const input of inputs){
     }
     input.classList.add('form-control')
 }
-
-const products = document.getElementById('products');
-
-products.addEventListener('click',function(event){
-        if (event.target.tagName == 'BUTTON'){
-            const btn = event.target
-            const icon = btn.firstElementChild
-            btn.classList.toggle('btn-outline-danger')
-            btn.classList.toggle('btn-success')
-            icon.classList.toggle('fa-trash')
-            
-
-            cart_badge = document.getElementById('cart_icon').querySelector('span')
-            if (icon.classList.contains('fa-trash')){
-                cart_badge.innerText = Number(cart_badge.innerText)+1;
-            }else{
-                cart_badge.innerText = Number(cart_badge.innerText)-1;
-            }
-
-        }
-        
-    })
